d3ad61c5f69fa0827614a8271e061c4eaa7b357a
debra68 <debradynamic@gmail.com>
2019-05-22 12:30:16 +0200

    mew

---
+++ FCC-Technical Documentation Page/FCC-Technical Documentation Page/JavaScript.js
@@ -0,0 +1,14 @@
+﻿function wordBlanks(myNoun, myAdjective, myVerb, myAdverb) {
+    // Your code below this line
+    var result = "";
+    result = "my name" + "" + myNoun + "i am" + "" + myAdjective + "i am a" + "" + myVerb + "i am very" + "" + myAdverb;
+
+    // Your code above this line
+
+    return result;
+}
+
+// Change the words here to test your function
+//something here
+wordBlanks("dog", "big", "ran", "quickly", "fastly");
+document.writeIn(result);
